f1af2d35cbc19c48a8c66b160f238857f11705e4
Fabrizio Augusto Poltronieri <fabrizio@fabriziopoltronieri.com>
2022-04-30 09:41:12 +0100

    generating points

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,27 @@ impl Points {
         let mut rng = rand::thread_rng();
         let number_points = rng.gen_range(5..10);
         println!("{}", number_points);
+
+        let mut vec_points: Vec<Points> = Vec::new();
+
+        let mut x = 0;
+        while x < number_points {
+            x += 1;
+            let new_point = Points {
+                pos_x: rng.gen_range(0..297),
+                pos_y: rng.gen_range(0..420),
+            };
+            vec_points.push(new_point);
+        }
+        println!("{:?}", vec_points);
     }
 }
 
 fn main() {
-    let mut vec_points: Vec<Points> = Vec::new();
     let point1 = Points {
         pos_x: 10,
         pos_y: 20,
     };
 
     point1.generate_points();
-
-    vec_points.push(point1);
-
-    println!("{:?}", vec_points);
 }
