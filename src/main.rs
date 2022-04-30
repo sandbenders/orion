9c376386c159996870ad1512cfb51179be685d67
Fabrizio Augusto Poltronieri <fabrizio@fabriziopoltronieri.com>
2022-04-30 09:25:59 +0100

    basic random implemented

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #[allow(dead_code, unused)]         // remove that later
+
+use rand::Rng;
+
 #[derive(Debug)]
 struct Points {
     pos_x: u16,                     // unsigned (0/+) max 65535
     pos_y: u16,
+}
+
+impl Points {
+    fn generate_points(&self) {
+        let mut rng = rand::thread_rng();
+        let number_points = rng.gen_range(5..10);
+        println!("{}", number_points);
+    }
 }
 
 fn main() {
@@ -11,6 +22,8 @@ fn main() {
         pos_x: 10,
         pos_y: 20,
     };
+
+    point1.generate_points();
 
     vec_points.push(point1);
 
