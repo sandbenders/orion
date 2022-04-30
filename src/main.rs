96bcd6f4655c65d70c9dce95d9a46faf4a804133
Fabrizio Augusto Poltronieri <fabrizio@fabriziopoltronieri.com>
2022-04-30 09:17:03 +0100

    basic vectors implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[allow(dead_code, unused)]         // remove that later
+#[derive(Debug)]
+struct Points {
+    pos_x: u16,                     // unsigned (0/+) max 65535
+    pos_y: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec_points: Vec<Points> = Vec::new();
+    let point1 = Points {
+        pos_x: 10,
+        pos_y: 20,
+    };
+
+    vec_points.push(point1);
+
+    println!("{:?}", vec_points);
 }
